4c56de985127a34fdf2e055aeea6d693d23795dd
HelloHelloHelloWorldWorld <1712131940@qq.com>
2020-07-01 23:02:50 +0800

    提交服务器端验证登录、注册代码，补：系统概要设计说明书；

--- TravelSocialServer/tp5/application/index/controller/Index.php
+++ TravelSocialServer/tp5/application/index/controller/Index.php
@@ -88,6 +88,48 @@ class Index extends Controller
         }
     }
 
- 
+    //用户注册
+    public function login()
+    {
+        $postJson = file_get_contents('php://input');
+        $jarray = json_decode($postJson, true);
+        if (empty($jarray)) {
+            //第一个if ,当$jarray的参数为空时
+            $data=array(
+                'text'=>'这是测试'
+            );
+            return returnJson('106','参数错误',$data);
+        }else{
+            //如果不是一个空的json数组
+            $result = $jarray['result'];//获取
+            $phone = $result['phone'];
+            $paswd = $result['password'];
+            $arrData = Db::query("select * from tb_user where phone='".$phone."'");
+            if (empty($arrData)){
+                //没有该账号，可以注册成功
+                $token=setAppLoginToken($phone);
+                $Nickname='昵称';//默认昵称
+                $date=date('Y-m-d H:i:s',time());
+                $login_user=Db::execute("INSERT INTO tb_user (phone, password,Nickname,datetime,code) VALUES ('".$phone."','".$paswd."','".$Nickname."','".$date."','".$token."')");
+                if(empty($login_user)){
+                    //新增用户失败
+                    $data=array();
+                    return returnJson('107','服务器错误',$data);
+                }else{
+                    //新增用户成功
+                    $data=array(
+                        'token'=>$token,
+                        'text'=>'这是测试'
+                    );
+                    return returnJson('104','注册成功',$data);
+                }
+            }else{
+                $data=array(
+                    'text'=>'这是测试'
+                );
+                return returnJson('105','账号已经被注册',$data);
+            }
+        }
+    }
 
 }
